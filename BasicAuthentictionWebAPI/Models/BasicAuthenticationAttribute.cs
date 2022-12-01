7c3bb56bdf54ec5f3bec7f8f30c3f42c841a702f
Noorjahan Shaik <noorjahan.iiit@gmail.com>
2022-12-01 14:53:56 +0000

    basic

---
+++ BasicAuthentictionWebAPI/Models/BasicAuthenticationAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace BasicAuthentictionWebAPI.Models
+{
+    public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        private const string Realm = "MyRealm";
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //If the Authorization header is empty or null
+            //then return Unauthorized
+            if(actionContext.Request.Headers.Authorization==null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+                // If the request was unauthorized, add the WWW-Authenticate header 
+                // to the response which indicates that it require basic authentication
+                if(actionContext.Response.StatusCode== System.Net.HttpStatusCode.Unauthorized )
+                {
+                    actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic Realm =\"{0}\"", Realm));
+                }
+            }
+            else
+            {
+                //Get the authentication token from the request header
+                string AuthToken = actionContext.Request.Headers.Authorization.Parameter;
+                //Decode the string
+                string DecodeAuthToken=Encoding.UTF8.GetString(Convert.FromBase64String(AuthToken));
+                //Convert the string into an string array
+                string[] usernamepasswordarray = DecodeAuthToken.Split(':');
+                //First element of the array is the username
+                string username= usernamepasswordarray[0];
+                //Second element of the array is the password
+                string password= usernamepasswordarray[1];
+                //call the login method to check the username and password
+                if(UserValidate.Login(username, password))
+                {
+                    var identity = new GenericIdentity(username);
+                    IPrincipal principal = new GenericPrincipal(identity, null);
+                    Thread.CurrentPrincipal=principal;
+                    if(HttpContext.Current != null)
+                    {
+                        HttpContext.Current.User=principal;
+                    }
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+            base.OnAuthorization(actionContext);
+        }
+    }
+}
---
+++ BasicAuthentictionWebAPI/Models/UserValidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BasicAuthentictionWebAPI.Models
+{
+    public class UserValidate
+    {
+        public static bool Login(string username, string password)
+        {
+            UserBusinessLayer UBL= new UserBusinessLayer();
+            var UserLists = UBL.GetUsers();
+            return UserLists.Any(user=> user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.Password.Equals(password, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
